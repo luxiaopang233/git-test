c18abd5af6b56e8402a587d8e88f52dc6d329b2b
Luxiaming <meruem@qq.com>
2022-02-24 16:34:27 +0800

    second-commit

--- src/main/java/com/luxiaopang/git/GitTest.java
+++ src/main/java/com/luxiaopang/git/GitTest.java
@@ -3,5 +3,7 @@ package com.luxiaopang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello GitHub");
+        System.out.println("Hello Gitee");
     }
 }
